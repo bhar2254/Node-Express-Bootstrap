15cf4eff67cd40c0527102e0fc1de4148d6603f2
Blaine Harper <danealue@gmail.com>
2023-03-18 18:02:18 -0500

    Update pet.js

--- routes/pet.js
+++ routes/pet.js
@@ -16,4 +16,14 @@ router.get('/child/:childName', function(req, res, next) {
 	res.render('child', { title: req.params.childName });
 });
 
+/* GET young adult page. */
+router.get('/young/:youngName', function(req, res, next) {
+	res.render('young', { title: req.params.youngName });
+});
+
+/* GET adult page. */
+router.get('/adult/:adultName', function(req, res, next) {
+	res.render('adult', { title: req.params.adultName });
+});
+
 module.exports = router;
