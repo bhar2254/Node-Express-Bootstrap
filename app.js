26dc8569314ca74809e541d9963a9ea74755202c
Blaine Harper <danealue@gmail.com>
2023-03-18 13:28:12 -0500

    update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var petRouter = require('./routes/pet');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
 
 app.use('/', indexRouter);
+app.use('/pet', petRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
