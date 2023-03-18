0ed72c0ea23ea7e298e7d93f46c3465402e79ce7
Blaine Harper <danealue@gmail.com>
2023-03-18 13:20:22 -0500

    Update pet.js

--- routes/pet.js
+++ routes/pet.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Petify' });
+	res.render('index', { title: 'Petify' });
 });
 /* GET home page. */
 router.get('/baby/:babyName', function(req, res, next) {
-  res.render('baby', { title: babyName });
+	console.log( req.params.babyName );
+	res.render('baby', { title: req.params.babyName });
 });
 
 module.exports = router;
