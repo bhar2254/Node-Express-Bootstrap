357da252fc89251d345885ab65053edd9bef7872
Blaine Harper <danealue@gmail.com>
2023-03-19 17:34:04 -0500

    Update view_pet.js

--- public/js/view_pet.js
+++ public/js/view_pet.js
@@ -1,7 +1,6 @@
 
 function changeMood(id, mood, age, name){
 	$(id).attr('src','/res/sprites/'+age+'/'+name+'/dynamic/'+mood+'.png');
-	setTimeout(resetMood(id, age, name), 400);
 }
 
 function resetMood(id, age, name){
