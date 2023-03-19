04e5b66d45b74a2ed7386b4bbbc9d92b431b3485
Blaine Harper <danealue@gmail.com>
2023-03-19 17:59:02 -0500

    Button bar on pet page

--- routes/pet.js
+++ routes/pet.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('pet', { title: 'Petify', name: req.query.name });
+	res.render('pet', { title: 'Petify', name: req.query.name, age: req.query.age });
 });
 
 /* GET baby page. */
