44557852c0345455e5bba0a12831fd7a6f7c3242
Blaine Harper <blaine.harper@indianhills.edu>
2023-03-20 10:54:36 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -36,9 +36,6 @@ router.post('/login', function(req, res, next) {
 			if (error) throw error;
 			// If the account exists
 			if (results.length > 0) {
-				// Authenticate the user
-				req.session.loggedin = true;
-				req.session.username = username;
 				// Redirect to home page
 				res.redirect('/home');
 			} else {
