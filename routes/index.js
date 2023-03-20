6b5a89808d37ea539363d1bf54261550851b8f61
Blaine Harper <blaine.harper@indianhills.edu>
2023-03-20 09:45:35 -0500

    added signup

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// Importing User Schema 
+const User = require('../public/js/user'); 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Petify' });
@@ -17,7 +20,66 @@ router.post('/login', function(req, res, next) {
 	let username = req.body.username;
 	let password = req.body.password;
 	res.send(`Username: ${username} Password: ${password}`);
+
+    // Find user with requested email 
+    User.findOne({ email : req.body.email }, function(err, user) { 
+        if (user === null) { 
+            return res.status(400).send({ 
+                message : "User not found."
+            }); 
+        } 
+        else { 
+            if (user.validPassword(req.body.password)) { 
+                return res.status(201).send({ 
+                    message : "User Logged In", 
+                }) 
+            } 
+            else { 
+                return res.status(400).send({ 
+                    message : "Wrong Password"
+                }); 
+            } 
+        } 
+    }); 
 });
+
+/* GET login page. */
+router.get('/signup', function(req, res, next) {
+	res.render('signup', { title: 'Signup' });
+});
+
+// User signup api 
+router.post('/signup', (req, res, next) => { 
+
+
+// Creating empty user object 
+    let newUser = new User(); 
+
+    // Initialize newUser object with request data 
+    newUser.name = req.body.name, 
+
+    newUser.email = req.body.email,
+
+
+    newUser.password=req.body.password
+
+                    // Call setPassword function to hash password 
+                    newUser.setPassword(req.body.password); 
+
+    // Save newUser object to database 
+    newUser.save((err, User) => { 
+        if (err) { 
+            return res.status(400).send({ 
+                message : "Failed to add user."
+            }); 
+        } 
+        else { 
+            return res.status(201).send({ 
+                message : "User added successfully."
+            }); 
+        } 
+    }); 
+}); 
 
 
 module.exports = router;
