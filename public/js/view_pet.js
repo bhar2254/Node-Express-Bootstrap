40cb6f0ec9683eaa4628e8f224fd9339a2648fde
Blaine Harper <danealue@gmail.com>
2023-03-19 19:31:09 -0500

    Update view_pet.js

--- public/js/view_pet.js
+++ public/js/view_pet.js
@@ -49,20 +49,28 @@ function changeStatus(id, status){
 	switch(status) {
 		case 'primary':
 			$(id).removeClass( "border-secondary border-success border-danger border-warning border-info border-light border-dark border-white" ).addClass( "border-primary" );
+			break;
 		case 'secondary':
 			$(id).removeClass( "border-primary border-success border-danger border-warning border-info border-light border-dark border-white" ).addClass( "border-secondary" );
+			break;
 		case 'success':
 			$(id).removeClass( "border-secondary border-primary border-danger border-warning border-info border-light border-dark border-white" ).addClass( "border-success" );
+			break;
 		case 'danger':
 			$(id).removeClass( "border-secondary border-success border-primary border-warning border-info border-light border-dark border-white" ).addClass( "border-danger" );
+			break;
 		case 'warning':
 			$(id).removeClass( "border-secondary border-success border-danger border-primary border-info border-light border-dark border-white" ).addClass( "border-warning" );
+			break;
 		case 'info':
 			$(id).removeClass( "border-secondary border-success border-danger border-warning border-primary border-light border-dark border-white" ).addClass( "border-info" );
+			break;
 		case 'light':
 			$(id).removeClass( "border-secondary border-success border-danger border-warning border-info border-primary border-dark border-white" ).addClass( "border-light" );
+			break;
 		case 'dark':
 			$(id).removeClass( "border-secondary border-success border-danger border-warning border-info border-light border-primary border-white" ).addClass( "border-dark" );
+			break;
 		case 'white':
 			$(id).removeClass( "border-secondary border-success border-danger border-warning border-info border-light border-dark border-primary" ).addClass( "border-white" );
 	}
