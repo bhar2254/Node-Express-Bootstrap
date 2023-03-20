7ce39129f1e552c47f97bc1d2187e85adc4152ab
Blaine Harper <blaine.harper@indianhills.edu>
2023-03-20 08:58:53 -0500

    login page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Petify' });
+	res.render('index', { title: 'Petify' });
 });
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+	res.render('login', { title: 'Login' });
+});
+
+/* POST home page. */
+router.post('/login', function(req, res, next) {
+	// Insert Login Code Here
+	let username = req.body.username;
+	let password = req.body.password;
+	res.send(`Username: ${username} Password: ${password}`);
+});
+
 
 module.exports = router;
