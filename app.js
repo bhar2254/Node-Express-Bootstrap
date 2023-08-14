0d40f6fbd01daeee35c0ca6361855890d84e2f1c
Blaine Harper <blaine.harper@indianhills.edu>
2023-08-14 12:48:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
 
 app.use('/', require('./routes/index'));
 app.use('/pet', require('./routes/pet'));
-app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
